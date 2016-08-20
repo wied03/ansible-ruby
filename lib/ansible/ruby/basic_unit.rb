039e55f45a04a316a69feef8a4fb69ba31538b9c
Brady Wied <brady@bswtechconsulting.com>
2016-08-20 15:09:40 -0600

    Basics and failing test

--- lib/ansible/ruby/basic_unit.rb
+++ lib/ansible/ruby/basic_unit.rb
@@ -1,6 +1,17 @@
 module Ansible
   module Ruby
     class BasicUnit
+      def initialize(args)
+      end
+
+      def to_h
+        {}
+      end
+
+      class << self
+        def attribute(*)
+        end
+      end
     end
   end
 end
--- spec/basic_unit_spec.rb
+++ spec/basic_unit_spec.rb
@@ -2,5 +2,26 @@ require 'spec_helper'
 require 'ansible-ruby'
 
 describe Ansible::Ruby::BasicUnit do
+  before { stub_const('EC2', klass) }
+  subject { instance.to_h }
+
+  context 'basic attributes' do
+    let(:klass) do
+      Class.new(Ansible::Ruby::BasicUnit) do
+        attribute :foo, required: true
+        attribute :bar, default: nil
+      end
+    end
+
+    context 'valid' do
+      let(:instance) { klass.new foo: 123 }
+
+      it { is_expected.to eq({'ec2' => {'foo' => 123 }}) }
+    end
+
+    context 'missing attribute' do
+      pending 'write this'
+    end
+  end
   pending 'write this'
 end
