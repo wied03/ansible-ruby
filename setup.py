9f70680dc5bc1f9ab3bac8b2f0d8a54cdcffd66f
Brady Wied <brady@bswtechconsulting.com>
2016-08-21 14:58:25 -0600

    Travis has an older dependency

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setuptools.setup(name='centos_package_cron',
       author_email='support@bswtechconsulting.com',
       url='https://github.com/wied03/ansible-ruby',
       packages=setuptools.find_packages(),
-      setup_requires=["ansible-lint", "ansible"],
+      setup_requires=["ansible-lint", "ansible", "pycrypto>=2.6"],
      )
