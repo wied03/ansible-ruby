1b9fb52e5f4cc60279240fc5b1108ad2f3eb039d
Brady Wied <brady@bswtechconsulting.com>
2016-08-20 20:12:38 -0600

    Need to figure out what to do about this case

--- spec/serializer_spec.rb
+++ spec/serializer_spec.rb
@@ -21,5 +21,22 @@ YAML
         is_expected.to eq text.rstrip
       end
     end
+
+    context 'null' do
+      let(:hash) do
+        { 'ec2' => { 'foo' => nil } }
+      end
+
+      it do
+        text = <<-YAML
+---
+# This is a generated YAML file by ansible-ruby, DO NOT EDIT
+ec2:
+  foo: null
+YAML
+        # Don't expecte a trailing CR
+        is_expected.to eq text.rstrip
+      end
+    end
   end
 end
