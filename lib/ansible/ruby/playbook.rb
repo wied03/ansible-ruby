c0f29831ed8a199a76f34aeb06e82d795b490150
Brady Wied <brady@bswtechconsulting.com>
2016-08-20 22:49:25 -0600

    Cover basic playbook case

--- lib/ansible/ruby/playbook.rb
+++ lib/ansible/ruby/playbook.rb
@@ -1,8 +1,31 @@
 require 'ansible/ruby/basic_unit'
+require 'ansible/ruby/task'
 
 module Ansible
   module Ruby
     class Playbook < BasicUnit
+      attribute :hosts, required: true, type: Array
+      attribute :tasks, type: [Array, Ansible::Ruby::Task]
+      attribute :roles, type: Array
+      attribute :connection, choices: [:local, :docker, :ssh]
+      attribute :user, type: String
+      attribute :serial, type: Integer
+      attribute :gather_facts, type: [TrueClass, FalseClass]
+
+      def to_h
+        result = super
+        hosts = result.delete 'hosts'
+        tasks = result.delete 'tasks'
+        # Be consistent with Ansible order
+        new_result = {
+          'hosts' => hosts.join(':'), # Ansible doesn't specify this as an array
+          'tasks' => tasks.is_a?(Array) ? tasks : [tasks] # ensure we have an array
+        }
+        result.each do |key, value|
+          new_result[key] = value
+        end
+        new_result
+      end
     end
   end
 end
--- spec/playbook_spec.rb
+++ spec/playbook_spec.rb
@@ -2,5 +2,44 @@ require 'spec_helper'
 require 'ansible-ruby'
 
 describe Ansible::Ruby::Playbook do
-  pending 'write this'
+  before do
+    stub_const('Ansible::Ruby::Modules::Ec2', module_klass)
+  end
+
+  let(:module_klass) do
+    Class.new(Ansible::Ruby::Modules::Base) do
+      attribute :foo, required: true
+    end
+  end
+
+  subject(:hash) { instance.to_h }
+
+  context 'basic' do
+    let(:task) { Ansible::Ruby::Task.new name: 'do stuff on EC2',
+                                         module: module_klass.new(foo: 123) }
+    let(:instance) { Ansible::Ruby::Playbook.new tasks: [task],
+                                                 hosts: %w(host1 host2) }
+
+    it do
+      is_expected.to eq({
+                          'hosts' => 'host1:host2',
+                          'tasks' => [
+                            {
+                              'name' => 'do stuff on EC2',
+                              'ec2' => {
+                                'foo' => 123
+                              }
+                            }
+                          ]
+                        })
+    end
+  end
+
+  context 'tasks and roles' do
+    pending 'write this, should be an error'
+  end
+
+  context 'roles only' do
+    pending 'write this'
+  end
 end
