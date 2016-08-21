60e4f77f352b0cfff301ecb123e59c73d8523185
Brady Wied <brady@bswtechconsulting.com>
2016-08-21 10:42:07 -0600

    4 rubocop offenses

--- lib/ansible-ruby.rb
+++ lib/ansible-ruby.rb
@@ -1,4 +1,7 @@
+# rubocop:disable Style/FileName
+# This is the name of the GEM, need it this way
 require 'ansible/ruby/version'
 require 'ansible/ruby/serializer'
 require 'ansible/ruby/dsl_builders/play'
 require 'ansible/ruby/modules/custom/foobar'
+# rubocop:enable Style/FileName
