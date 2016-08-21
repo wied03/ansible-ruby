a75f63cb1f54e337c4dbff125f0a452fa6d5af2a
Brady Wied <brady@bswtechconsulting.com>
2016-08-21 14:14:58 -0600

    Fix serializer issue, verify with real Rake task

--- Rakefile
+++ Rakefile
@@ -27,9 +27,9 @@ rule '.yml' => '.rb' do |t|
   puts "Updating #{t.name}"
   require 'ansible-ruby'
   ruby = File.read t.source
-  play_builder = Ansible::Ruby::DslBuilders::Play.new
-  play = play_builder.evaluate ruby
-  yml = Ansible::Ruby::Serializer.serialize play.to_h
+  playbook_builder = Ansible::Ruby::DslBuilders::Playbook.new
+  playbook = playbook_builder.evaluate ruby
+  yml = Ansible::Ruby::Serializer.serialize playbook.to_h
   File.write t.name, yml
 end
 
--- lib/ansible/ruby/serializer.rb
+++ lib/ansible/ruby/serializer.rb
@@ -4,10 +4,17 @@ module Ansible
   module Ruby
     module Serializer
       class << self
-        def serialize(hash)
+        def serialize(value)
           # Don't want symbols in YAML
-          str = hash.deep_stringify_keys.to_yaml
-          lines = str.split "\n"
+          value = case value
+                  when Hash
+                    value.deep_stringify_keys
+                  when Array
+                    value.map { |item| item.deep_stringify_keys }
+                  else
+                    raise "Unknown type #{value.class}"
+                  end
+          lines = value.to_yaml.split "\n"
           lines.insert 1, '# This is a generated YAML file by ansible-ruby, DO NOT EDIT'
           lines.join "\n"
         end
