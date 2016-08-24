336dda95a422a76be8f739b5960ba41742b2f795
Brady Wied <brady@bswtechconsulting.com>
2016-08-24 10:10:02 -0600

    More example tinkering

--- examples/webservers.rb
+++ examples/webservers.rb
@@ -1,19 +1,15 @@
 play 'webserver install' do
-  hosts 'host1'
-
-  task 'install nginx' do
-    yum do
-      name 'nginx'
-      state :absent
-    end
-  end
+  local_host
 
   task 'EC2 load balancer' do
     ec2_elb_lb do
       state :present
       name 'lb_01'
-      security_group_ids %w(id1 id2)
-      listeners({22 => 'tcp'})
+      security_group_ids %w(sg-3c4fca59 sg-cabcc0ad)
+      # TODO: This should either fail since it's not an array or should be put into an array behind the scenes
+      listeners protocol: 'http',
+                load_balancer_port: 80,
+                instance_port: 80
     end
   end
 end
