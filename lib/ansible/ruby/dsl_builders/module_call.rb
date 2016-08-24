4542e3dc4e562d54254873be8219630fffdd6cbb
Brady Wied <brady@bswtechconsulting.com>
2016-08-24 17:15:05 -0600

    DSL changes for free form

--- lib/ansible/ruby/dsl_builders/module_call.rb
+++ lib/ansible/ruby/dsl_builders/module_call.rb
@@ -1,6 +1,7 @@
 require 'ansible/ruby/dsl_builders/base'
 require 'ansible/ruby/dsl_builders/args'
 require 'ansible/ruby/modules/base'
+require 'ansible/ruby/modules/custom/free_form'
 
 module Ansible
   module Ruby
@@ -19,12 +20,21 @@ module Ansible
           id.to_s.capitalize
         end
 
-        def process_method(id, *, &block)
+        def process_method(id, *module_args, &block)
           raise "Unknown module #{id}" unless respond_to_missing? id, true
           module_klass = MODULES_MOD.const_get klass_name(id)
+          free_form_module = module_klass.include?(Ansible::Ruby::Modules::FreeForm)
+          free_form_arg = if module_args.any?
+                            raise "Can't use arguments #{module_args} on this type of module" unless free_form_module
+                            raise 'Expected only 1 argument for this type of module' if module_args.length > 1
+                            module_args[0]
+                          else
+                            raise 'Expected 1 argument for this type of module' if free_form_module
+                          end
           # Delegate everything to the args builder and apply it to the module class we located
           module_builder = Args.new
           args = module_builder.evaluate(&block)
+          args[:free_form] = free_form_arg if free_form_arg
           @result = module_klass.new(args)
           @result.validate!
         end
