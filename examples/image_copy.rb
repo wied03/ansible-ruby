0a1b92f964822ee6817151330848e280d45c3da4
Brady Wied <brady@bswtechconsulting.com>
2016-08-24 13:19:29 -0600

    Add another example to test so called flat hashes

---
+++ examples/image_copy.rb
@@ -0,0 +1,12 @@
+play 'image copy' do
+  local_host
+
+  task 'image copy' do
+    ec2_ami_copy do
+      source_region 'us-east-1'
+      source_image_id 'ami-1b9e2570'
+      name 'test'
+      wait :no
+    end
+  end
+end
