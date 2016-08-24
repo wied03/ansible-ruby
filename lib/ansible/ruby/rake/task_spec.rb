181095b1d3bce6a58c25cb3a790123a60f0fb49f
Brady Wied <brady@bswtechconsulting.com>
2016-08-23 19:16:41 -0600

    Keep our test from depending on the generated code

--- lib/ansible/ruby/rake/task_spec.rb
+++ lib/ansible/ruby/rake/task_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 require 'ansible-ruby'
 require 'ansible/ruby/rake/task'
+require 'rake/copy'
 
 describe Ansible::Ruby::Rake::Task do
   let(:rake_dir) { 'spec/rake/no_nested_tasks' }
