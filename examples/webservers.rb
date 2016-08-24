862bb283716c5f67a5022667f3704c38a2e62cef
Brady Wied <brady@bswtechconsulting.com>
2016-08-24 12:34:58 -0600

    it worked

--- examples/webservers.rb
+++ examples/webservers.rb
@@ -4,11 +4,12 @@ play 'webserver install' do
   task 'EC2 load balancer' do
     ec2_elb_lb do
       state :present
-      name 'lb_01'
+      name 'lb-01'
       security_group_ids %w(sg-3c4fca59 sg-cabcc0ad)
       listeners protocol: 'http',
                 load_balancer_port: 80,
                 instance_port: 80
+      subnets 'subnet-637e594b'
     end
   end
 end
