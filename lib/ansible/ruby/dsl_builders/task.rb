bdbab6fd5fcf10f8da51a64ffe714eab2402a16c
Brady Wied <brady@bswtechconsulting.com>
2016-08-26 11:53:55 -0600

    Extract out into a base class

--- lib/ansible/ruby/dsl_builders/task.rb
+++ lib/ansible/ruby/dsl_builders/task.rb
@@ -2,69 +2,15 @@ require 'ansible/ruby/dsl_builders/base'
 require 'ansible/ruby/dsl_builders/module_call'
 require 'ansible/ruby/dsl_builders/result'
 require 'ansible/ruby/models/task'
+require 'ansible/ruby/dsl_builders/unit'
 
 module Ansible
   module Ruby
     module DslBuilders
-      class Task < Base
+      class Task < Unit
         def initialize(name, context)
-          super()
-          @context = context
+          super
           @module = nil
-          @temp_counter = 0
-          @name = name
-          @task_args = {}
-        end
-
-        def become(*args)
-          value = _implicit_bool args
-          @task_args[:become] = value
-        end
-
-        def become_user(value)
-          @task_args[:become_user] = value
-        end
-
-        def changed_when(clause)
-          @task_args[:changed_when] = clause
-        end
-
-        def failed_when(clause)
-          @task_args[:failed_when] = clause
-        end
-
-        def ansible_when(clause)
-          @task_args[:when] = clause
-        end
-
-        def with_dict(clause)
-          @task_args[:with_dict] = clause
-          return unless block_given?
-          hash_key = JinjaItemNode.new('item.key')
-          hash_value = JinjaItemNode.new('item.value')
-          yield [hash_key, hash_value]
-        end
-
-        def with_items(clause)
-          @task_args[:with_items] = clause
-          yield JinjaItemNode.new if block_given?
-        end
-
-        def async(value)
-          @task_args[:async] = value
-        end
-
-        def poll(value)
-          @task_args[:poll] = value
-        end
-
-        def notify(value)
-          @task_args[:notify] = value
-        end
-
-        def ignore_errors(*args)
-          value = _implicit_bool args
-          @task_args[:ignore_errors] = value
         end
 
         def respond_to_missing?(*)
---
+++ lib/ansible/ruby/dsl_builders/unit.rb
@@ -0,0 +1,67 @@
+# see LICENSE.txt in project root
+module Ansible
+  module Ruby
+    module DslBuilders
+      class Unit < Base
+        def initialize(name, context)
+          super()
+          @context = context
+          @temp_counter = 0
+          @name = name
+          @task_args = {}
+        end
+
+        def become(*args)
+          value = _implicit_bool args
+          @task_args[:become] = value
+        end
+
+        def become_user(value)
+          @task_args[:become_user] = value
+        end
+
+        def changed_when(clause)
+          @task_args[:changed_when] = clause
+        end
+
+        def failed_when(clause)
+          @task_args[:failed_when] = clause
+        end
+
+        def ansible_when(clause)
+          @task_args[:when] = clause
+        end
+
+        def with_dict(clause)
+          @task_args[:with_dict] = clause
+          return unless block_given?
+          hash_key = JinjaItemNode.new('item.key')
+          hash_value = JinjaItemNode.new('item.value')
+          yield [hash_key, hash_value]
+        end
+
+        def with_items(clause)
+          @task_args[:with_items] = clause
+          yield JinjaItemNode.new if block_given?
+        end
+
+        def async(value)
+          @task_args[:async] = value
+        end
+
+        def poll(value)
+          @task_args[:poll] = value
+        end
+
+        def notify(value)
+          @task_args[:notify] = value
+        end
+
+        def ignore_errors(*args)
+          value = _implicit_bool args
+          @task_args[:ignore_errors] = value
+        end
+      end
+    end
+  end
+end
