9296c593a4582253d5f64718807378dced309f68
Brady Wied <brady@wied.us>
2017-03-14 11:33:02 -0600

    get_url fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 * `ec2` corrections for `count_tag`, `ebs_optimized`, `instance_profile_name`, `exact_count` parameters
 * `ec2_ami`, `ec2_vol`, `iam_policy`, `iam`, `ec2_eip` correction for region
 * `ec2_ami` `state` parameter validation correction
+* `get_url` support for `owner`, `group`, `mode`, `setype`, `selevel`
 
 # 1.0.6 - 14 October 2016
 * Allow sharing variables between tasks (#29)
--- lib/ansible/ruby/modules/custom/network/basics/get_url.rb
+++ lib/ansible/ruby/modules/custom/network/basics/get_url.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 # VALIDATED_CHECKSUM: v5T3dbovbGBE0ljyTcaYnW+gG0ChWYC8CHAat1jZPP8=
 require 'ansible/ruby/modules/generated/core/network/basics/get_url'
+require 'ansible/ruby/modules/helpers/file_attributes'
 
 module Ansible
   module Ruby
     module Modules
       class Get_url
+        include Helpers::FileAttributes
         remove_existing_validations :headers
         # @return [Hash, nil] Add custom HTTP headers to a request in the format "key:value,key:value"
         validates :headers, type: Hash
