7744e4013d61e265880915d3f32abcf21945d4c8
Brady Wied <brady@wied.us>
2017-03-14 12:03:45 -0600

    Do not need this gem inheritance anymore

--- lib/ansible/ruby/modules/custom/core/packaging/language/gem.rb
+++ lib/ansible/ruby/modules/custom/core/packaging/language/gem.rb
@@ -7,7 +7,7 @@ require 'ansible/ruby/modules/generated/core/packaging/language/gem'
 module Ansible
   module Ruby
     module Modules
-      class Gem < Gem
+      class Gem
         remove_existing_validations :user_install
         validates :user_install, inclusion: { :in => [:yes, :no], :message => "%{value} needs to be :yes, :no" }, allow_nil: true
       end
