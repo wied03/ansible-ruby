4dfccab0f3698e52cada25fbd6b0d7cffb7ac0c2
Brady Wied <brady@bswtechconsulting.com>
2019-05-15 00:09:02 -0600

    Add missing require

--- lib/ansible/ruby/modules/custom/files/lineinfile.rb
+++ lib/ansible/ruby/modules/custom/files/lineinfile.rb
@@ -3,6 +3,7 @@
 # VALIDATED_CHECKSUM: eNy9dt5MNTxeoSLUAttIX4/ZhU+wqY6uO5uPETNv3d4=
 # see LICENSE.txt in project root
 
+require 'ansible/ruby/modules/generated/files/lineinfile'
 require 'ansible/ruby/modules/helpers/file_attributes'
 
 module Ansible
