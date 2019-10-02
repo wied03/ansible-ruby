a6b084bc1766d90ab081341ea2ea5e897d19eba8
Brady Wied <brady@bswtechconsulting.com>
2019-10-02 14:31:39 -0600

    Ensure either array or non array case works

--- lib/ansible/ruby/modules/custom/packaging/language/pear.rb
+++ lib/ansible/ruby/modules/custom/packaging/language/pear.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # VALIDATED_CHECKSUM: aIM1dczpLFU2xMaIijCkTmi54/yEPZKaE3hO5Lja5bo=
+
 require 'ansible/ruby/modules/generated/packaging/language/pear'
 
 module Ansible
--- lib/ansible/ruby/modules/custom/packaging/language/pear_spec.rb
+++ lib/ansible/ruby/modules/custom/packaging/language/pear_spec.rb
@@ -5,16 +5,28 @@ require 'spec_helper'
 
 describe Ansible::Ruby::Modules::Pear do
   subject(:mod) do
-    Ansible::Ruby::Modules::Pear.new name: %w[package1 package2]
+    Ansible::Ruby::Modules::Pear.new name: packages
   end
 
   describe '#to_h' do
     subject { mod.to_h }
 
-    it do
-      is_expected.to eq pear: {
-        name: 'package1,package2'
-      }
+    context 'array' do
+      let(:packages) { %w[package1 package2] }
+      it do
+        is_expected.to eq pear: {
+          name: 'package1,package2'
+        }
+      end
+    end
+
+    context 'single' do
+      let(:packages) { 'package1' }
+      it do
+        is_expected.to eq pear: {
+          name: 'package1'
+        }
+      end
     end
   end
 end
